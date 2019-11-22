497819eb344f03f1f8f373cc7dec009ba99cf759
codeItRalf <codeItRalf@users.noreply.github.com>
2019-11-23 00:02:25 +0100

    added dropdown and fixed category

--- www/js/App.js
+++ www/js/App.js
@@ -22,8 +22,8 @@ class App {
       'omoss': new AboutUs(),
       'page404': new Page404(),
       'cart' : new Cart()
-     
     };
+
     // A shop should always have a cart
     this.cart = new Cart();
     // Listen to hash changes - rerender...
@@ -37,9 +37,15 @@ class App {
     let hash = location.hash.replace(/#/g, '');
     // The first part of the hash is everything before a '-' character
     let hashFirstPart = hash.split('-')[0];
+
+    //split hash at "&" second string is subcategory
+    this.subCategory = hash.split('&')[1];
+    //Each time the method gets called filter and make new list the sub category from the list which is created in loadProducts()
+    this.routes.produkter = new ProductList(this.products.filter(product => product.category ==  this.subCategory));
+  
     // Look up the "page to show" - the instance to call render on
     // if we do not find any page set the page to 'page404'
-    let pageToShow = this.routes[hash] || this.routes.page404;
+    let pageToShow = this.routes[hash.split('&')[0]] || this.routes.page404;
     // Make the correct menu item active
     // (the css selector finds a-tags with matching hrefs)
     $('header nav a').removeClass('active');
@@ -59,13 +65,14 @@ class App {
     this.products = [];
     // Loop through the JSON data and create Products
     for (let productData of productsData) {
-      let product = new Product(productData, this.cart);
-      this.products.push(product);
-      this.routes[product.slug] = product;
+          let product = new Product(productData, this.cart);
+          this.products.push(product);
+          this.routes[product.slug] = product;
+         
     }
     // Make a new product list with all of our products
     // and add it to our routes
-    this.routes.produkter = new ProductList(this.products);
+    
     // Now we are ready to call changeRoute and display
     // the correct page on initial page load..
     this.changeRoute();
