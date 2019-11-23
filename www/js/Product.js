f854347aa36502fd81282c498d5a3dbe371c55ef
Snehal Patel <snehal.kathiriya@gmail.com>
2019-11-23 20:41:15 +0100

    a

--- www/js/Product.js
+++ www/js/Product.js
@@ -32,6 +32,8 @@ class Product {
       // from doing what it normally does on a certain element
       // since the buy button is sometimes inside a a-tag
       // in this case it prevents us from following the a-tag
+      let key = this.id;
+      localStorage.setItem(key,JSON.stringify(this));
       e.preventDefault();
       // this.cart is an instance of Cart
       // add me to that cart
