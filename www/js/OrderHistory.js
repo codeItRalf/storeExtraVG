0e7cd24abc1322df2bf7d1c83e4a01cfd4f05e82
Ralf <Raffe_83@hotmail.com>
2019-12-09 16:09:16 +0100

    added some responsive fixes history

--- www/js/OrderHistory.js
+++ www/js/OrderHistory.js
@@ -7,7 +7,7 @@ class OrderHistory {
   render() {
 
     $("main").html(/*html*/ `
-        <div class="container">
+        <div class="">
       <div class="row d-flex flex-column">
       
           <div class="col-12 d-flex flex-row justify-content-around">
@@ -17,15 +17,13 @@ class OrderHistory {
             <span class="wide-item customer-location">Location</span>
             <div class="history-sorter mx-auto">
             <div class="wide-item order-date-lg d-flex justify-content-center" id="toggle-order">
-            <span> Order Date</span>
+            <span>Order Date</span>
             <div>
-             ${this.sort ? '<div class="fas fa-sort-up sort-history"></div>' : '<div class="fas fa-sort-down sort-history"></div>'} 
+             ${this.sort ? ' <span class="oi oi-data-transfer-upload sort-history"></span>' : ' <span class="oi oi-data-transfer-download sort-history"></span>'} 
             </div>
           
             </div>
             </div>
-            
-            <span class="wide-item order-date-sm"><i class="fas fa-calendar-day"></i></span>
             <span class="wide-item delivery-status">Status</span>
             <span class="wide-item">Amount</span>
           </div>
@@ -65,22 +63,26 @@ class OrderHistory {
   renderInList(order) {
     // This is how I render myself in a list of products
     // (this method is called from a ProductList)
-    return /*html*/ `<section class="list-group-item col-12 d-flex flex-row justify-content-around pl-0 pr-0">
-        <div class="wide-item detail-button"><a
+    return /*html*/ `<section class="list-group-item col-12 d-flex flex-row justify-content-between pl-0 pr-0">
+        
+        <div class=" wide-item detail-button"><a
             title="View Details"
-            data-toggle="tooltip">${order.orderNumber}</a></div>
+            data-toggle="tooltip">${order.orderNumber}</a>
+        </div>
+
+        <div class="d-flex flex-row justify-content-around pl-0 pr-0">   
         <div class="wide-item customer">
         <span>${order.customer.firstName} ${order.customer.lastName}</span>
           
         </div>
         <div class="wide-item customer-adress">${order.customer.adress}</div>
         <div class="wide-item customer-location">${order.customer.country}</div>
-        <div class="wide-item order-date-lg">${this.unixToDate(
+        <div class="wide-item order-date">${this.unixToDate(
           parseInt(order.orderNumber) + 3600000
         )}</div>
         <div class="wide-item delivery-status"><span class="status text-warning">&bull;</span> Pending</div>
         <div class="wide-item">${order.cart.cartValue.totalPrice}€</div>
-        
+        </div>
       </section>`;
   }
 
